86683ae8cb29d1d8b77fdb46620a64067b295aaf
Brendan Wood <brendan.wood@concordia.ca>
2024-09-03 08:45:27 -0400

    Add project files.

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result = 0;
+
+            if (argOperation == "+" || argOperation.ToLower() == "add") {
+                result = argFirstNumber + argSecondNumber;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            return 0;
+        }
+    }
+}
