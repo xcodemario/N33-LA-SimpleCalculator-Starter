e0a27bd408cc13978ef812bf60bc5d4f08b80279
Jake Tremblay <2330184@champlaincollege.qc.ca>
2024-09-03 13:29:47 -0400

    Worked on Change 5, adding all the classes, and an extra one that is exponents

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -7,11 +7,40 @@ namespace SimpleCalculator
         public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
+            switch (argOperation) {
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
-                result = argFirstNumber + argSecondNumber;
+                case "+": 
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+            
+            
+                case "-":
+                case "minus":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "/":
+                case "divide":
+                case "division":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                case "*":
+                case "multiply":
+                case "multiplication":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "power to":
+                case "power of":
+                case "exponent":
+
+                    break;
+
             }
 
+            
             return result;
         }
     }
