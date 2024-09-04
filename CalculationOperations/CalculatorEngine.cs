f3f6c2b0cb55262224fbc2420af5ff67513a2100
Jake Tremblay <2330184@champlaincollege.qc.ca>
2024-09-04 10:52:25 -0400

    DLL

---
+++ CalculationOperations/CalculatorEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result = 0;
+            switch (argOperation) {
+
+                case "+": 
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+            
+            
+                case "-":
+                case "minus":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "/":
+                case "divide":
+                case "division":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                case "*":
+                case "multiply":
+                case "multiplication":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "power to":
+                case "power of":
+                case "exponent":
+
+                    break;
+
+            }
+
+            return result;
+        }
+    }
+}
