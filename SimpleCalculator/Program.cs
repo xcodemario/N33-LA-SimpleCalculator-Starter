849b59b6f0820ce424e96215c9b9b17422a39a4c
Jake Tremblay <2330184@champlaincollege.qc.ca>
2024-09-04 11:16:38 -0400

    Worked on Change 3 by adding human readable output

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -18,8 +18,8 @@ namespace SimpleCalculator
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine("Result: " + result);
-
+                Console.WriteLine(firstNumber + operation + secondNumber + " is equal to " + result);
+                Console.ReadKey();
             } catch (Exception ex)
             {
        
@@ -41,7 +41,8 @@ namespace SimpleCalculator
                 }
                 catch (ArgumentException)
                 {
-                    Console.WriteLine("enter a valide numeric value.");
+                    Console.WriteLine("enter a valid numeric value.");
+                    
                 }
             }
             return number;
@@ -58,17 +59,21 @@ namespace SimpleCalculator
                     case "+":
                     case "plus":
                         return "+";
+                        break;
                     case "-":
                     case "minus":
                         return "-";
+                        break;
                     case "*":
                     case "times":
                         return "*";
+                        break;
                     case "/":
                     case "divide":
                         return "/";
+                        break;
                     default:
-                        Console.WriteLine("enter a valide operation");
+                        Console.WriteLine("enter a valid operation");
                         break;
                 }
             }
